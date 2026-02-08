0322fd12ac02edbd0f83726f43feeff8a7f17735
DeveloperZeeshu <itsabbaszeeshaan0604@gmail.com>
2026-02-08 13:46:19 +0530

    Bug fixes

--- app/shop/(pages)/dashboard/page.tsx
+++ app/shop/(pages)/dashboard/page.tsx
@@ -84,7 +84,7 @@ export default function ShopDashboard() {
                         </button>
 
                         <Link
-                            href='/shop/manage-products'
+                            href='/shop/manage-products?cursor=&limit=10&dir=next'
                             className="px-3 py-2 rounded-lg bg-white backdrop-blur active:scale-95 transition border border-gray-300 hover:bg-gray-50">
                             View All Products
                         </Link>
